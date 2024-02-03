3c4c4c3dd746336bcf02473fff5e6b4b595c450a
Jose Villarreal <jvillrr@amazon.com>
2024-02-03 14:57:07 -0800

    Begin CDK for apprunner for phonebook backend

---
+++ lib/phonebook_infra-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import apprunner = require('@aws-cdk/aws-apprunner-alpha'); // Allows working with App Runner resources
+import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets'; // Allows building the docker image and uploading to ECR
+import * as path from "path"; // Helper for working with file paths
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class PhonebookInfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    
+    new apprunner.Service(this, 'Service', {
+      source: apprunner.Source.fromGitHub({
+        repositoryUrl: 'https://github.com/phonebookApp/backend',
+        branch: 'main',
+        configurationSource: apprunner.ConfigurationSourceType.API,
+        codeConfigurationValues: {
+          runtime: apprunner.Runtime.NODEJS_14,
+          port: '3000',
+          startCommand: 'npm start',
+        },
+        connection: apprunner.GitHubConnection.fromConnectionArn('arn:aws:apprunner:us-west-2:656805403368:connection/Github-ppvillarreal/149a900acde445208d8dbf9d89c67d38'),
+      }),
+    });
+  }
+}
