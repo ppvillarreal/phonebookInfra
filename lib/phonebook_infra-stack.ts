616465bbe31f1dcae60b6d0bb9a60b0e17e0dd7a
Jose Villarreal <jvillrr@amazon.com>
2024-02-03 15:53:33 -0800

    Add build command instruction for apprunner

--- lib/phonebook_infra-stack.ts
+++ lib/phonebook_infra-stack.ts
@@ -1,8 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import apprunner = require('@aws-cdk/aws-apprunner-alpha'); // Allows working with App Runner resources
-import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets'; // Allows building the docker image and uploading to ECR
-import * as path from "path"; // Helper for working with file paths
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PhonebookInfraStack extends cdk.Stack {
@@ -18,6 +16,7 @@ export class PhonebookInfraStack extends cdk.Stack {
           runtime: apprunner.Runtime.NODEJS_14,
           port: '3001',
           startCommand: 'npm start',
+          buildCommand: 'npm install'
         },
         connection: apprunner.GitHubConnection.fromConnectionArn('arn:aws:apprunner:us-west-2:656805403368:connection/Github-ppvillarreal/149a900acde445208d8dbf9d89c67d38'),
       }),
