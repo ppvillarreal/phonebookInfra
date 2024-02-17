c577ad834fb3ff22109dd0e000e8f8f35ae7302c
Jose Villarreal <jvillrr@amazon.com>
2024-02-16 18:01:15 -0800

    Add DynamoDB table to app infra

--- lib/phonebook_infra-stack.ts
+++ lib/phonebook_infra-stack.ts
@@ -1,13 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import apprunner = require('@aws-cdk/aws-apprunner-alpha'); // Allows working with App Runner resources
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apprunner from '@aws-cdk/aws-apprunner-alpha'; 
+import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb'; 
+import { Role, ServicePrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class PhonebookInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create a DynamoDB table
+    const table = new Table(this, 'PhonebookTable', {
+      partitionKey: { name: 'id', type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST, // Use on-demand billing mode
+    });
     
-    new apprunner.Service(this, 'Service', {
+    // Create an IAM role for App Runner
+    const appRunnerRole = new Role(this, 'AppRunnerAccessRole', {
+      assumedBy: new ServicePrincipal('tasks.apprunner.amazonaws.com'),
+      description: 'Allows App Runner service to access DynamoDB',
+    });
+
+    // Grant the role permissions to access the DynamoDB table
+    table.grantReadWriteData(appRunnerRole);
+    // This is not meant for production, so we will let table be destroyed
+    table.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+
+    // Create the App Runner service
+    const service = new apprunner.Service(this, 'Service', {
       source: apprunner.Source.fromGitHub({
         repositoryUrl: 'https://github.com/ppvillarreal/phonebookApp',
         branch: 'main',
@@ -16,10 +35,25 @@ export class PhonebookInfraStack extends cdk.Stack {
           runtime: apprunner.Runtime.NODEJS_14,
           port: '3001',
           startCommand: 'npm start',
-          buildCommand: 'npm install'
+          buildCommand: 'npm install',
         },
         connection: apprunner.GitHubConnection.fromConnectionArn('arn:aws:apprunner:us-west-2:656805403368:connection/Github-ppvillarreal/149a900acde445208d8dbf9d89c67d38'),
       }),
+      // Associate the IAM role with the App Runner service
+      instanceRole: appRunnerRole,
+    });
+
+    // Add environment variables to connect to DynamoDB
+    service.addEnvironmentVariable("DYNAMODB_TABLE_NAME", table.tableName)
+    service.addEnvironmentVariable("DYNAMODB_TABLE_ARN", table.tableArn)
+
+    // Optionally, output the DynamoDB table name and ARN
+    new cdk.CfnOutput(this, 'TableName', {
+      value: table.tableName,
+    });
+
+    new cdk.CfnOutput(this, 'TableArn', {
+      value: table.tableArn,
     });
   }
 }
